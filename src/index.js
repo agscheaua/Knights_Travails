b7c89f1e428dee5d597f6af669ffde08a9960865
Alex <agscheaua@gmail.com>
2026-09-23 23:03:42 +0300

    i need to create the logic to evaluate each node/ move in breadth first search order and to enqueue the children of a level to be run after there are no more nodes/ moves in that level

--- src/index.js
+++ src/index.js
@@ -1,41 +1,145 @@
 // import './styles.css'
 
-let pathToEnd = [];
-
 function knightMoves(startCoordinates, endCoordinates) {
-  let allNodePossibleMoves = [];
-  allNodePossibleMoves.push([startCoordinates[0] - 1, startCoordinates[1] - 2]);
-  allNodePossibleMoves.push([startCoordinates[0] - 2, startCoordinates[1] - 1]);
-  allNodePossibleMoves.push([startCoordinates[0] - 2, startCoordinates[1] + 1]);
-  allNodePossibleMoves.push([startCoordinates[0] - 1, startCoordinates[1] + 2]);
-  allNodePossibleMoves.push([startCoordinates[0] + 1, startCoordinates[1] + 2]);
-  allNodePossibleMoves.push([startCoordinates[0] + 2, startCoordinates[1] + 1]);
-  allNodePossibleMoves.push([startCoordinates[0] + 2, startCoordinates[1] - 1]);
-  allNodePossibleMoves.push([startCoordinates[0] + 1, startCoordinates[1] - 2]);
+  if (String(startCoordinates) === String(endCoordinates)) {
+    return String([startCoordinates]);
+  };
+  
+  function getAllPossibleMoves(currentLocation) {
+    
+    let allPossibleMoves = [];
+    allPossibleMoves.push([currentLocation[0] - 1, currentLocation[1] - 2]);
+    allPossibleMoves.push([currentLocation[0] - 2, currentLocation[1] - 1]);
+    allPossibleMoves.push([currentLocation[0] - 2, currentLocation[1] + 1]);
+    allPossibleMoves.push([currentLocation[0] - 1, currentLocation[1] + 2]);
+    allPossibleMoves.push([currentLocation[0] + 1, currentLocation[1] + 2]);
+    allPossibleMoves.push([currentLocation[0] + 2, currentLocation[1] + 1]);
+    allPossibleMoves.push([currentLocation[0] + 2, currentLocation[1] - 1]);
+    allPossibleMoves.push([currentLocation[0] + 1, currentLocation[1] - 2]);
 
-  let allVALIDPossibleMoves = [];
-  for (let i = 0; i < allNodePossibleMoves.length; i++) {
-    if ((allNodePossibleMoves[i][0] >= 0 && allNodePossibleMoves[i][0] < 8) &&
-        (allNodePossibleMoves[i][1] >= 0 && allNodePossibleMoves[i][1] < 8)) {
-      allVALIDPossibleMoves.push(allNodePossibleMoves[i]);
+    let allVALIDPossibleMoves = [];
+    for (let i = 0; i < allPossibleMoves.length; i++) {
+      if ((allPossibleMoves[i][0] >= 0 && allPossibleMoves[i][0] < 8) &&
+          (allPossibleMoves[i][1] >= 0 && allPossibleMoves[i][1] < 8)) {
+        allVALIDPossibleMoves.push(String(allPossibleMoves[i]));
+      };
     };
-  };
 
-  if (allVALIDPossibleMoves.length === 0) {
-    throw new Error ("The knight cannot reach that sqare!");
+    return allVALIDPossibleMoves;
   };
+  const allPossibleMoves = getAllPossibleMoves(startCoordinates);
+  console.log(allPossibleMoves);
 
-  for (let j = 0; j < allVALIDPossibleMoves.length; j++) {
-    if (String(allVALIDPossibleMoves[j]) === String(endCoordinates)) {
+  let nodesToBeEval = [];
+  let visitedNodes = [];
+
+  for (let i = 0; i < allPossibleMoves.length; i++) {
+    nodesToBeEval.push(allPossibleMoves[i]);
+  };
+  console.log(nodesToBeEval);
+
+  let loopBreaker = 0
+  while(nodesToBeEval.length !== 0) {
+    loopBreaker++
+    if (loopBreaker > 30) {
+      console.log("force break");
       break;
     };
+
+    if (nodesToBeEval[0] === String(endCoordinates)) {
+      console.log("endCoord find");
+      break;
+    } else if (nodesToBeEval[0] !== String(endCoordinates)) {
+        console.log("....")
+    }
+
+
   };
 
-  pathToEnd.push([startCoordinates]);
-  pathToEnd[startCoordinates] = [allVALIDPossibleMoves];
 
-  console.log(pathToEnd);
+  /*
+  let visitedNodes = [];
+  let queueOfNodeToBeVisited = [];
+
+  for (let i = 0; i < initialMoves.length; i++) {
+    queueOfNodeToBeVisited.push(initialMoves[i]);
+  };
+
+  function checkIfNodeIsEndCoord(node) {
+    if (node === String(endCoordinates)) return true;
+    return false;
+  };
+
+  function checkIfNodeWasVisited(node) {
+    if (visitedNodes.includes(node)) return true;
+    return false;
+  };
+
+  function enqueueNodeChildren(node) {
+    const nodeChildren = getAllPossibleMoves(node);
+    for (let i = 0; i < nodeChildren.length; i++) {
+      if (checkIfNodeWasVisited(nodeChildren[i]) === true) continue;
+      else {
+        queueOfNodeToBeVisited.push(nodeChildren[i]);
+      };
+    };
+  };
+
+  function evaluateNodes() {
+    for (let i = 0; queueOfNodeToBeVisited.length !== 0; i++) {
+      if (checkIfNodeIsEndCoord(queueOfNodeToBeVisited[0]) === true) break;
+      if (checkIfNodeWasVisited(queueOfNodeToBeVisited[0]) === true) {
+        continue;
+      } else {
+        visitedNodes.push(queueOfNodeToBeVisited[0]);
+      };
+      enqueueNodeChildren(queueOfNodeToBeVisited[0]);
+      queueOfNodeToBeVisited.unshift();
+    };  
+  };
+  evaluateNodes();
+  */
+
+
+  /*
+  let queueOfNodeToBeEval = [];
+  for (let i = 0; i < possibleMoves.length; i++) {
+    queueOfNodeToBeEval.push(possibleMoves[i]);
+  };
+  
+  let alreadyEvalNode = [];
+  function evaluateNodes(currMoves) {
+    for (let i = 0; queueOfNodeToBeEval !== 0; i++) {
+      if (String(queueOfNodeToBeEval[i]) === String(endCoordinates)) {
+        break;
+      };
+      if (String(queueOfNodeToBeEval[i]) !== String(endCoordinates)) {
+        if (alreadyEvalNode.length === 0) {
+          alreadyEvalNode.push(String(queueOfNodeToBeEval[i]));
+        } else {
+            if (alreadyEvalNode.includes(String(queueOfNodeToBeEval[i]))) {
+              continue;
+            } else {
+              alreadyEvalNode.push(String(queueOfNodeToBeEval[i]))
+            };
+        };
+      };
+      const possibleMovesOfCurrentNode = getAllPossibleMoves(queueOfNodeToBeEval[0]);
+      for (let j = 0; j < possibleMovesOfCurrentNode.length; i++) {
+        queueOfNodeToBeEval.push(possibleMovesOfCurrentNode[j]);
+      };
+      
+      queueOfNodeToBeEval.unshift();
+
+      console.log(queueOfNodeToBeEval);
+    };
+  };
+  evaluateNodes()
+  */
+
 };
 
-console.log(knightMoves([0,0], [3, 3]));
+console.log(knightMoves([0, 0], [3, 3]));
+
+
 
